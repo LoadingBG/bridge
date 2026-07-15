68926ef850c0dd88a92acb7aa371c2510389c2ec
Loading BG <troyankru@gmail.com>
2026-07-16 01:07:35 +0300

    Include component stylesheet automatically

--- systems/createComponent.js
+++ systems/createComponent.js
@@ -4,6 +4,10 @@ export default async function createComponent(name, classCreator) {
   const file = await fetch(`./components/${name}.html`);
   const html = await file.text();
   const template = PARSER.parseFromString(html, "text/html").querySelector("template").content;
+  const stylesheet = document.createElement("link");
+  stylesheet.setAttribute("rel", "stylesheet");
+  stylesheet.setAttribute("href", `./components/${name}.css`);
+  template.prepend(stylesheet);
   const componentClass = classCreator(template);
   customElements.define(name, componentClass);
 }
